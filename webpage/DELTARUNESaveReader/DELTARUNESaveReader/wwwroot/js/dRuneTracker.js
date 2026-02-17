9e0e96e4caa469208dda6be0cc61230eb8b1208c
krabaa <krabaa@confederationcollege.ca>
2026-02-17 10:36:02 -0500

    Moved scripts to the html page at the teacher's suggestion.

--- webpage/DELTARUNESaveReader/DELTARUNESaveReader/wwwroot/js/dRuneTracker.js
+++ webpage/DELTARUNESaveReader/DELTARUNESaveReader/wwwroot/js/dRuneTracker.js
@@ -4,10 +4,4 @@ const input = document.getElementById('saveInput');
 
 const inButton = document.getElementById("confirmButton");
 inButton.addEventListener("click", readFile);
-function readFile() {
-    alert("Is this even working")
-    docIdGet("saveConfirmer").innerText = "This is the element that i need to edit.";
-}
 
-
-alert('Is this even reading?');
