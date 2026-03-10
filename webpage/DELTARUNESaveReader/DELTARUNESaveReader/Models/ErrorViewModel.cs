c387abe53d6f93c377485dc9a46728a9c07626e2
sillycat41 <panduhh41@gmail.com>
2026-03-10 11:09:44 -0400

    Removed all files except working files, adding .gitignore

--- webpage/DELTARUNESaveReader/DELTARUNESaveReader/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DELTARUNESaveReader.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
